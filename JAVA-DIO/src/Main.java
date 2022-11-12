242e0943a0ec8023487e840963bd132410cffe23
mirela cazelato <m.angelocazelato@gmail.com>
2022-11-12 00:41:00 -0300

    Boas práticas Java

---
+++ JAVA-DIO/src/Main.java
@@ -0,0 +1,46 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i;
+        int I;
+        int m;
+        int a;
+
+        i= 5;
+        I= 10;
+        m= 20;
+        a= 7;
+
+        final int j = 10 ;
+
+        int numero;
+        int outroNumero = 10;
+
+        numero = 100;
+        outroNumero= 10;
+
+        int quantidadeProduto= 50;
+        final int NUMERO_TENTATIVA = 5;
+        int quantidadeOpcoes = 25;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(m);
+        System.out.println(a);
+
+        System.out.println(j);
+        System.out.println(numero);
+        System.out.println(outroNumero);
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVA);
+        System.out.println(quantidadeOpcoes);
+
+
+
+
+
+
+    }
+}
